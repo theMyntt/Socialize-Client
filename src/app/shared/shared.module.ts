bb5bc3093a013d9fb33a9213bd1b5d4644af58ec
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-19 13:51:51 -0300

    chore: implementing at shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
-  declarations: [ButtonComponent],
+  declarations: [ButtonComponent, InputComponent],
   imports: [CommonModule],
-  exports: [ButtonComponent],
+  exports: [ButtonComponent, InputComponent],
 })
 export class SharedModule {}
