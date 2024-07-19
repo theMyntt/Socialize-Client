b1bab2c0d8c9f51042537fa1ba1036e2f84d67d4
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-19 17:15:39 -0300

    chore: implementing features module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
+import { FeaturesModule } from './features/features.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, CoreModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    CoreModule,
+    FeaturesModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
