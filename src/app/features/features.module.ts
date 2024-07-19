007716591cd6e64ca9ec77e42a6087800e6616ec
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-19 17:20:25 -0300

    feat: base login module and component

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeModule } from './home/home.module';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HomeModule],
+  imports: [CommonModule, HomeModule, LoginModule],
   exports: [HomeModule],
 })
 export class FeaturesModule {}
